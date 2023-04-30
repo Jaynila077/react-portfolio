b2d640cd541617ab1f34093792b05d14b1f486a1
Jaynila077 <jaynila077@gmail.com>
2023-04-30 13:40:51 +0530

    Home Page added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
       <Router>
         <Routes>
-          <Route path="/"  />
+          <Route path="/" />
         </Routes>
       </Router>
     </div>
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,21 @@
+export const Home = () => {
+    return (
+        <div className="home">
+            <nav>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+           <h1 className="one">
+            Hi, I'm <span id="jay">Jay</span>
+           </h1>
+          <p>
+           Web Devlopment &amp; <br />
+           Data Science Enthusiast
+         </p>
+        </div>
+    )
+}
