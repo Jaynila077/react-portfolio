3b1da1d32e3a81612bf7d951a3c147f9ffcde788
Jaynila077 <jaynila077@gmail.com>
2023-05-01 15:50:53 +0530

    added card component

---
+++ portfolio/src/componets/Card.js
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+export const Card = () => {
+  const cardRef = useRef(null);
+  const buttonRef = useRef(null);
+  const headingRef = useRef(null);
+  const picRef = useRef(null);
+  const socialRef = useRef(null);
+  const iconsRef = useRef([]);
+  const textRef = useRef(null); 
+  const arrowRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      paused: true,
+    });
+
+    tl.to(headingRef.current, { fontSize: "48px" }, )
+      .to(buttonRef.current, { scale: 16.5 }, "-=0.2")
+      .to(textRef.current, { color: "#fff" }, "-=0.2")
+      .to(arrowRef.current, { opacity: 1 , color:"#fff"}, "-=0.2")
+      .to(picRef.current, { filter: "grayscale(0)" }, "-=0.2")
+      .to(socialRef.current, { opacity: 1, y: 0, stagger: 0.1 }, "-=0.4")
+
+    const cardEl = cardRef.current;
+
+    const handleMouseEnter = () => {
+      tl.play();
+    };
+
+    const handleMouseLeave = () => {
+      tl.reverse();
+    };
+
+    cardEl.addEventListener("mouseenter", handleMouseEnter);
+    cardEl.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      cardEl.removeEventListener("mouseenter", handleMouseEnter);
+      cardEl.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  return (
+     <div className="container">
+        <div className="card" ref={cardRef}>
+      <h2 ref={headingRef}>Project1</h2>
+      <i ref={arrowRef}className="fas fa-arrow-right"></i>
+      <p ref={textRef}>some text.</p>
+      <div className="pic" ref={picRef}></div>
+      <div className="social" ref={socialRef}>
+        <i ref={(el) => (iconsRef.current[0] = el)} className="fab fa-github"></i>
+      </div>
+      <button ref={buttonRef}></button>
+    </div>
+     </div>
+  );
+};
+
+
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,9 +1,11 @@
 import { Navbar } from "./Navbar"
+import { Card } from "../componets/Card"
 export const Projects = () => {
     return (
         <div className="projects">
             <Navbar />
             <h1>Projects</h1>
+            <Card/>
         </div>    
     )
 }
